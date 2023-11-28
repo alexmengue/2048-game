faf4d6af380498abfd8b56d5c01c092f5c9672e2
Alex <alexmengue1990@hotmail.com>
2023-11-28 01:21:56 -0300

    att

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,14 @@ function setGame () {
     [0, 0, 0, 0],
     [0, 0, 0, 0]
   ]
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      let tile = document.createElement('div');
+      tile.id = r.toString() + '-' + c.toString();
+      let num = board[r][c];
+      updateTile(tile, num);
+    }
+  }
 };
+
