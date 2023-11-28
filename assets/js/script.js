df51e9e3c9ed2f180095ebab73b347e8f0d295bb
Alex <alexmengue1990@hotmail.com>
2023-11-27 21:21:58 -0300

    att board JS

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+let board;
+let score;
+let rows = 4;
+let columns = 4;
+
+window.onload = () => {
+  setGame();
+};
+
+function setGame () {
+  board = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]
+};
