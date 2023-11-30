d865b10a32e60ec1d819fb0779d1af85f5968624
Alex <alexmengue1990@hotmail.com>
2023-11-30 00:25:04 -0300

    started the logic to merge the numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ window.onload = () => {
   setGame();
 };
 
-function setGame () {
+function setGame() {
   board = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -37,6 +37,50 @@ function updateTile(tile, num) {
       tile.classList.add('x' + num.toString());
     } else {
       tile.classList.add('x8192');
+    }
+  }
+}
+
+document.addEventListener('keyup', (event) => {
+  if (event.code === 'ArrowLeft') {
+    slideLeft();
+  }
+});
+
+function filterZero(row) {
+  return row.filter(num => num !== 0);
+}
+
+function slide(row) {
+  row = filterZero(row);
+
+  for (let i = 0; i < row.length - 1; i++) {
+    if (row[i] === row[i + 1]) {
+      row[i] *= 2;
+      row[i + 1] = 0;
+      score += row[i];
+    }
+  }
+
+  row = filterZero(row);
+
+  while (row.length < columns) {
+    row.push(0);
+  }
+
+  return row;
+}
+
+function slideLeft() {
+  for (let r = 0; r < rows; r++) {
+    let row = board[r];
+    row = slide(row);
+    board[r] = row;
+
+    for (let c = 0; c < columns; c++) {
+      let tile = document.getElementById(r.toString() + '-' + c.toString());
+      let num = board[r][c];
+      updateTile(tile, num);
     }
   }
 }
