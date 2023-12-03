d312349044af543d8ae742f3ba7dbc25a04761ee
Alex <alexmengue1990@hotmail.com>
2023-12-03 15:30:01 -0300

    slide up done

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,10 @@ document.addEventListener('keyup', (event) => {
   else if (event.code === 'ArrowRight') {
     slideRight();
   }
+
+  else if (event.code === 'ArrowUp') {
+    slideUp();
+  }
 });
 
 function filterZero(row) {
@@ -106,7 +110,20 @@ function slideRight() {
 }
 
 function slideUp() {
-  //soon
+  for (let c = 0; c < columns; c++) {
+    let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+    row = silde(row);
+    board[0][c] = row[0];
+    board[1][c] = row[1];
+    board[2][c] = row[2];
+    board[3][c] = row[3];
+
+    for (let r = 0; r < rows; r++) {
+      let tile = document.getElementById(r.toString() + '-' + c.toString());
+      let num = board[r][c];
+      updateTile(tile, num);
+    }
+  }
 }
 
 function slideDown() {
